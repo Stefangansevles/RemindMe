e7665756c059f889085d59e9b4d9a34c931549ab
StefanGansevles <stefanogansevles@hotmail.com>
2021-08-15 11:24:29 +0200

    version number update

--- RemindMe/Properties/AssemblyInfo.cs
+++ RemindMe/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.06")]
-[assembly: AssemblyFileVersion("3.1.06")] 
+[assembly: AssemblyVersion("3.1.07")]
+[assembly: AssemblyFileVersion("3.1.07")] 
 [assembly: NeutralResourcesLanguage("en")]
